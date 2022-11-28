90045b4b1e0b716c5f0b7119378ce58eea24ac68
Evan <ejh2998@gmail.com>
2022-11-28 03:48:06 -0500

    some component styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import ExpenseItem from "./components/expenseItem";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
